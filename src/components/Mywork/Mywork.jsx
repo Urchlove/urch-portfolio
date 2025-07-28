e68db70531508a32a5c246c281c755c013169738
Urchlove <136825399+Urchlove@users.noreply.github.com>
2025-07-28 13:22:40 +0100

    new commit

--- src/components/Mywork/Mywork.jsx
+++ src/components/Mywork/Mywork.jsx
@@ -5,6 +5,7 @@ import mywork from '../../assets/project-data'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
 
+
 function Mywork() {
   return (
     <div id='Portfolio' className='mywork'>
@@ -19,6 +20,13 @@ function Mywork() {
             return <img key={index} src= {work.w_img} alt="" />
 
           })}
+        </div>
+        <div className='aa'>
+          <h1>MY PROJECTS LINKS</h1>
+          <a href="https://edu-web-demo.vercel.app/" target='_blank'>Please click Link 1</a>
+          <a href="https://urch-best-birthday.vercel.app/" target='_blank'>Please click Link 2</a>
+          <a href='https://portfolio-wine-alpha-20.vercel.app/' target='_blank'>Please Click Link 3</a>
+
         </div>
         <div className="mywork-showmore">
           <p>Show More</p>
